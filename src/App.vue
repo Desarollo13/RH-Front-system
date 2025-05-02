b82b48196ffcc921d8d87c36c85704e9821d4a0b
Desarrollo-8 <desarrollo.8@nexen-elog.com>
2025-05-02 15:57:16 -0600

    login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh">
-    <h1 class="text-success text-center">Mi primer proyecto</h1>
-  </div>
+  <router-view />
 </template>
 
 <script setup></script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Login from '@/views/pages/login.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/',
+      name: 'login',
+      component: Login,
+    },
   ],
 })
 
