ac2407e67aa7bb36248a922f2fcdfa135b3bc886
Desarrollo-8 <desarrollo.8@nexen-elog.com>
2025-05-02 16:18:59 -0600

    validaciones y responsive login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/pages/login.vue'
+import Login from '@/views/pages/login/login.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
