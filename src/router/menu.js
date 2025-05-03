4aa7461575a952bf5cec8ab032f054e4be08f175
Desarrollo-8 <desarrollo.8@nexen-elog.com>
2025-05-02 18:42:36 -0600

    side bar completado

--- src/router/menu.js
+++ src/router/menu.js
@@ -1,1 +1,61 @@
-export default {}
+export const menuItems = [
+  {
+    title: 'Inicio',
+    path: '/dashboard',
+    icon: 'bi bi-house',
+  },
+  {
+    title: 'Recursos Humanos',
+    path: '#',
+    icon: 'bi bi-star-fill',
+    submenus: [
+      {
+        title: 'Contrataciones',
+        path: '#',
+        icon: 'bi bi-star-fill',
+      },
+      {
+        title: 'Empleados',
+        path: '#',
+        icon: 'bi bi-star-fill',
+      },
+      {
+        title: 'Evaluaciones',
+        path: '#',
+        icon: 'bi bi-star-fill',
+      },
+      {
+        title: 'Nomina',
+        path: '#',
+        icon: 'bi bi-wallet',
+      },
+      {
+        title: 'Tickets',
+        path: '#',
+        icon: '/bi bi-ticket-detailed',
+      },
+    ],
+  },
+  {
+    title: 'Usuarios',
+    path: '#',
+    icon: 'bi bi-person',
+  },
+  {
+    title: 'Configuración',
+    path: '#',
+    icon: 'bi bi-gear',
+    submenus: [
+      {
+        title: 'Roles',
+        path: '#',
+        icon: 'bi bi-person-fill',
+      },
+      {
+        title: 'Permisos',
+        path: '#',
+        icon: 'bi bi-person-lines-fill',
+      },
+    ],
+  },
+]
