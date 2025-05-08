5a37093fa2050fb95873588d2d680d43fd1bfac4
DevNexen-6 <desarrollo.6@nexen-elog.com>
2025-05-08 14:27:26 -0600

    modificacion/ dependencias

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import '@/assets/root.css'
 
 import { createApp } from 'vue'
@@ -9,10 +10,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createBootstrap } from 'bootstrap-vue-next'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createBootstrap()) 
 
 app.mount('#app')
