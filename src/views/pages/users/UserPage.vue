de2cfc361db4de524ff4db064850081a4412b522
Desarrollo-8 <desarrollo.8@nexen-elog.com>
2025-05-12 18:52:22 -0600

    Vista de usuarios lista para el backend

--- src/views/pages/users/UserPage.vue
+++ src/views/pages/users/UserPage.vue
@@ -18,6 +18,9 @@
       @edit="editItem"
       @delete="deleteItem"
     />
+
+    <!-- Modal controlado por la vista -->
+    <ModalCreateEdit :visible="showModal" @close="showModal = false" />
   </CardContainer>
 </template>
 
@@ -25,7 +28,10 @@
 import { ref, onMounted } from 'vue';
 import CardContainer from '@/components/utils/CardContainer.vue';
 import DataTable from '@/components/data-table/DataTable.vue';
+import ModalCreateEdit from './components/ModalCreateEdit.vue';
 import { usePageTitle } from '@/composables/usePageTitle.js';
+
+const showModal = ref(false);
 
 const pageTitle = usePageTitle();
 onMounted(() => (pageTitle.value = 'Ejemplo'));
@@ -34,7 +40,6 @@ const search = ref('');
 const perPage = ref(10);
 const currentPage = ref(1);
 
-//datos simulados
 const columns = [
   { key: 'id', label: 'ID' },
   { key: 'name', label: 'Nombre' },
@@ -43,15 +48,14 @@ const columns = [
 ];
 
 const items = ref([
-  { id: 1, name: 'Juan Pérez', email: 'juan@example.com', rol: 'administrador' },
+  { id: 1, name: 'Juan Pérez', email: 'juan@example.com', rol: 'administrador', status: 1 },
   { id: 2, name: 'Ana Gómez', email: 'ana@example.com', rol: 'administrador', status: 1 },
-  { id: 3, name: 'Carlos Ruiz', email: 'carlos@example.com', rol: 'administrador' },
-  { id: 3, name: 'Janeth Fuentes', email: 'jan@example.com', rol: 'administrador', status: 1 },
+  { id: 3, name: 'Carlos Ruiz', email: 'carlos@example.com', rol: 'administrador', status: 1 },
+  { id: 4, name: 'Janeth Fuentes', email: 'jan@example.com', rol: 'administrador', status: 1 },
 ]);
 
-//funciones de acciones
 function handleCreate() {
-  alert('Crear nuevo usuario');
+  showModal.value = true;
 }
 
 function editItem(item) {
